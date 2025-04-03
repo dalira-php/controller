8563224665efe4ac5a6e458a27ea9142610190c3
ONESYSTEAM <info.onesysteam@gmail.com>
2025-04-03 13:20:58 +0800

    Updated

---
+++ Generator.php
@@ -0,0 +1,42 @@
+<?php
+
+if ($argc < 3) {
+    echo "Usage: composer create-controller CONTROLLER_NAME MODEL_NAME\n";
+    exit(1);
+}
+
+$controllerName = ucfirst($argv[1]);
+$modelName = ucfirst($argv[2]);
+
+$controllerContent = <<<PHP
+<?php
+
+namespace app\Controllers;
+
+use config\DBConnection;
+use app\Models\$modelName;
+
+class {$controllerName}Controller
+{
+    private \${$modelName};
+
+    public function __construct()
+    {
+        \$DBConnection = new DBConnection();
+        \$this->{$modelName} = new {$modelName}(\$DBConnection);
+    }
+}
+PHP;
+
+$path = getcwd() . "/app/Controllers/{$controllerName}Controller.php";
+
+if (!file_exists(dirname($path))) {
+    mkdir(dirname($path), 0777, true);
+}
+
+if (!file_exists($path)) {
+    file_put_contents($path, $controllerContent);
+    echo "{$controllerName}Controller.php created successfully in app/Controllers\n";
+} else {
+    echo "{$controllerName}Controller.php already exists. Skipping...\n";
+}
